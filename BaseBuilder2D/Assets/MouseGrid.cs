7a434c08ed426bd366881400256673f5596a7938
Somdat <tsomda@gmail.com>
2017-01-16 14:23:21 -0800

    Click to add a tower. Enemy Spawner and Nexus Base

---
+++ BaseBuilder2D/Assets/MouseGrid.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseGrid : MonoBehaviour {
+
+    public float snapValue = 1;
+    public float depth = 0;
+
+    public GameObject square;
+    public Transform mapTiles;
+
+    public List<Vector3> placedTiles;
+
+    private SpriteRenderer sprite;
+
+    // Use this for initialization
+    void Start () {
+        placedTiles = new List<Vector3>();
+
+        sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //var pos = Input.mousePosition;
+        //transform.position = GetGridPos(pos);
+
+        SnapPos();
+
+        if(placedTiles.Contains(transform.position))
+        {
+            sprite.color = Color.red;
+            return;
+        }
+        else
+        {
+            sprite.color = Color.white;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(transform.position);
+            PlaceSquare();
+        }
+
+    }
+
+    void PlaceSquare()
+    {
+        Instantiate(square, transform.position, Quaternion.identity, mapTiles);
+        placedTiles.Add(transform.position);
+    }
+
+    void SnapPos()
+    {
+        var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float snapInverse = 1 / snapValue;
+
+        float x, y, z;
+
+        // if snapValue = .5, x = 1.45 -> snapInverse = 2 -> x*2 => 2.90 -> round 2.90 => 3 -> 3/2 => 1.5
+        // so 1.45 to nearest .5 is 1.5
+        x = Mathf.Round(pos.x * snapInverse) / snapInverse;
+        y = Mathf.Round(pos.y * snapInverse) / snapInverse;
+        z = depth + 10;  // depth from camera
+
+        transform.position = new Vector3(x, y, z);
+    }
+}
---
+++ BaseBuilder2D/Assets/TheGrid.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheGrid : MonoBehaviour {
+
+    public int gridX;
+    public int gridY;
+    public GameObject tile;
+
+
+    private Camera cam;
+
+    private float mapX;
+    private float mapY;
+
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
+
+    private float screenWidth;
+    private float screenHeight;
+
+    // Use this for initialization
+    void Start () {
+        UpdateCameraValues();
+
+        //var square = Instantiate(tile, GetPosition(0, 0), Quaternion.identity, transform);
+        //square.transform.position = new Vector3(maxX, minY, 10f);
+
+        
+
+        for (int i = 0; i < gridX; i++)
+        {
+            for(int j = 0; j < gridY; j++)
+            {
+                Instantiate(tile, GetPosition(i, j), Quaternion.identity, transform);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    Vector3 GetPosition(int i, int j)
+    {
+        var posX = maxX + (i);
+        var posY = minY - (j);
+
+        var pos = new Vector2(posX, posY);
+
+        return pos;
+    }
+
+
+    void UpdateCameraValues()
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+
+        var vertExtent = Camera.main.GetComponent<Camera>().orthographicSize;
+        var horzExtent = vertExtent * Screen.width / Screen.height;
+
+        // Calculations assume map is position at the origin
+        minX = horzExtent - mapX / 2.0f;
+        maxX = mapX / 2.0f - horzExtent;
+        minY = vertExtent - mapY / 2.0f;
+        maxY = mapY / 2.0f - vertExtent;
+    }
+}
