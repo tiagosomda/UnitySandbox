c810c210ee4c23b4f381aeffe72fdc5a3b4a94b8
Somdat <tsomda@gmail.com>
2017-01-15 22:40:44 -0800

    Added BaseDefender (free form tower defense) - 3d path finding with dynamic obstacles

---
+++ BaseDefender/Assets/EnemyPointClick.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPointClick : MonoBehaviour {
+
+    private Camera cam;
+    private NavMeshAgent nav;
+
+
+    public Transform waypoint1;
+    public Transform waypoint2;
+
+    private Transform target;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        nav = GetComponent<NavMeshAgent>();
+
+        target = waypoint2;
+
+        nav.SetDestination(target.position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.tag.Equals("Node"))
+                {
+                    hit.collider.gameObject.GetComponent<MeshRenderer>().enabled = !hit.collider.gameObject.GetComponent<MeshRenderer>().enabled;
+                    hit.collider.gameObject.GetComponent<NavMeshObstacle>().enabled = !hit.collider.gameObject.GetComponent<NavMeshObstacle>().enabled;
+                }
+            }
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= 0.3f)
+        {
+            if (target == waypoint1)
+            {
+                target = waypoint2;
+            }
+            else
+            {
+                target = waypoint1;
+            }
+
+            nav.SetDestination(target.position);
+        }
+    }
+}
